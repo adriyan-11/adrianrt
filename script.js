95c8cf8ba62aa8b7b78b7ef036fcbf3b2ccb14c5
iant101 <147980037+iant101@users.noreply.github.com>
2025-02-22 21:51:27 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
-const mainMenu = document.querySelector('.mainMenu');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.openMenu');
-const menu_items = document.querySelectorAll('nav .mainMenu li a');
 
+const mainMenu = document.querySelector(".mainMenu")
+const closeMenu = document.querySelector(".closeMenu")
+const openMenu = document.querySelector(".openMenu")
+const menu_items = document.querySelectorAll("nav .mainMenu li a")
 
+openMenu.addEventListener("click", show)
+closeMenu.addEventListener("click", close)
 
-
-openMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
-
-// close menu when you click on a menu item 
-menu_items.forEach(item => {
-    item.addEventListener('click',function(){
-        close();
-    })
+// Close menu when you click on a menu item
+menu_items.forEach((item) => {
+  item.addEventListener("click", () => {
+    close()
+  })
 })
 
-function show(){
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
+function show() {
+  mainMenu.style.display = "flex"
+  mainMenu.style.top = "0"
 }
-function close(){
-    mainMenu.style.top = '-100%';
+
+function close() {
+  mainMenu.style.top = "-100%"
 }
+
+// Add resize event listener to handle menu visibility on screen size change
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 800) {
+    mainMenu.style.display = "flex"
+    mainMenu.style.top = "0"
+  } else {
+    mainMenu.style.display = "none"
+    mainMenu.style.top = "-100%"
+  }
+})
+
+
